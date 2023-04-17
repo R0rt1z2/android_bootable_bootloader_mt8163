4e3aef18f95ef00cd52ae1aa77a63f94da2ce2c7
R0rt1z2 <me@r0rt1z2.com>
2023-04-17 17:02:57 +0200

    [lk]: Update README.md and build script

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# android_littlekernel_aquaris_m8
-Custom LK ("little kernel") for the BQ Aquaris M8
+# Bootloader for MT8163 based devices
+This repository contains the source code for the bootloader (LK) of MT8163 devices. To build the bootloader, please follow the instructions below.
+
+## Dependencies
+Before building the bootloader, you need to install the following dependencies:
+```bash
+sudo dpkg --add-architecture i386
+sudo apt-get update
+sudo apt-get install libc6:i386 libstdc++6:i386 zlib1g:i386
+```
+
+## Building the Bootloader
+To build the bootloader, follow the steps below:
+1. Clone the repository to your local machine.
+2. Navigate to the repository directory.
+3. Run the build script with the target device name as a parameter (`./build.sh <target>`).
--- build.sh
+++ build.sh
@@ -1,16 +1,43 @@
 #!/bin/bash
 
+#
+# This shouldn't be changed since this LK
+# is part of the Android 6.0.1 BSP and other
+# toolchain(s) will probably cause unexpected
+# build issue(s).
+#
 ANDROID_VERSION="marshmallow-release"
 
-if [ $# -eq 0 ]
-  then
-    echo "[-] No arguments supplied"
+# Make sure we have one target at least.
+if [ $# -eq 0 ]; then
+  echo "USAGE: '$0 <target>'"
+  exit 1
 fi
 
-echo "[?] Building for $1"
-rm -rf build-$1 >/dev/null 2>&1
-[ -d "arm-linux-androideabi-4.9" ] || git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/ -b $ANDROID_VERSION --depth=1
-make TOOLCHAIN_PREFIX=./arm-linux-androideabi-4.9/bin/arm-linux-androideabi- PROJECT:=$1 | tee ../logs/build-log-$(date +%y%m%d%H%M)
+# If we got a target, save it.
+TARGET=$1
 
-echo "[+] All jobs done"
-exit 0
+# If present, delete old build directorie(s) and log(s).
+rm -rf build-$TARGET* > /dev/null 2>&1
+
+# If not present, clone the toolchain.
+if [ ! -d 'arm-linux-androideabi-4.9' ]; then
+  echo "[?] Cloning the toolchain, please wait..."
+  git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b $ANDROID_VERSION --depth=1 > /dev/null 2>2&1 # Use depth=1 because we don't care about the history.
+fi
+
+# Start the build, use the proper toolchain and the supplied build target.
+echo "[?] Building LK for target $1... This may take a while, please wait..."
+make TOOLCHAIN_PREFIX=./arm-linux-androideabi-4.9/bin/arm-linux-androideabi- PROJECT:=$TARGET > build-$TARGET.txt 2>&1
+
+# Check if the command executed fine.
+if [ ! -f "build-$TARGET/lk.bin" ]; then
+  echo "[!] Something went horribly wrong while trying to build for $1. Please check log(s) for more details!"
+  exit 1
+fi
+
+# If we're here, that means everything succeed.
+echo "[+] Successfully built LK for target $1!"
+echo "[?] LK image  = $(readlink -f build-$TARGET/lk.bin)"
+echo "[?] LK md5sum = $(md5sum build-$TARGET/lk.bin)"
+echo "[?] LK size   = $(stat -c%s build-$TARGET/lk.bin)"
